b41e8cad7417c9fa40e61c7e3661366b9724bda2
Lado <Tsivtsivadzelado@yahoo.com>
2020-12-08 01:16:25 -0800

    Initial Commit

---
+++ davaleba-12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  davaleba-12
+//
+//  Created by Ladolado3911 on 12/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
